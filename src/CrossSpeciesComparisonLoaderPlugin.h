c2c04be7efdb7a338ee3ab9e691710c95b86722c
Soumyadeep Basu <44787782+basusoumyadeep@users.noreply.github.com>
2024-03-07 14:35:46 +0100

    include fix

--- src/CrossSpeciesComparisonLoaderPlugin.h
+++ src/CrossSpeciesComparisonLoaderPlugin.h
@@ -6,7 +6,7 @@
 #include <QJsonObject>
 #include <QJsonDocument>
 #include <QJsonArray>
-#include "CrossSpeciesComparisonTreeData/CrossSpeciesComparisonTreeData.h"
+#include "CrossSpeciesComparisonTreeData.h"
 #include "InfoSettingsAction.h"
 using namespace mv::plugin;
 
